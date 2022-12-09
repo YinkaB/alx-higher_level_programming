1fd05b9e53c3e18988bdcf3f1b5956abfe82c5ba
YinkaB <yinkababatolu@yahoo.com>
2022-12-09 13:21:53 +0100

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    length = len(sys.argv) - 1
-    if length == 0:
-        print("0 arguments.")
-    elif length == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(length))
+    i = len(sys.argv) - 1
 
-    for i in range(length):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
